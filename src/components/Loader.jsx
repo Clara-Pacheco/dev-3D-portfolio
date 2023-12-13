4f1ae3455a916a1d11df92d491006fb2b0f62e49
Clara <clarapachecodev@gmail.com>
2023-12-13 10:28:24 -0300

    Create a spinning animation in loader component using tailwind

--- src/components/Loader.jsx
+++ src/components/Loader.jsx
@@ -1,3 +1,7 @@
 export const Loader = () => {
-  return <div></div>;
+  return (
+    <div className="flex justify-center items-center">
+      <div className="w-20 h-20 border-2 border-opacity-20 border-blue-500 border-t-blue-500 rounded-full animate-spin" />
+    </div>
+  );
 };
