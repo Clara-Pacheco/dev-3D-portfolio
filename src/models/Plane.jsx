0fffe556f9797a89d1628812b02367d020a06457
Clara <clarapachecodev@gmail.com>
2023-12-18 17:32:31 -0300

    Add a console.log to check the value of isRotating

--- src/models/Plane.jsx
+++ src/models/Plane.jsx
@@ -14,6 +14,7 @@ export function Plane({ isRotating, ...props }) {
   // Use an effect to control the plane's animation based on 'isRotating'
   // Note: Animation names can be found on the Sketchfab website where the 3D model is hosted.
   useEffect(() => {
+    console.log({ isRotating });
     if (isRotating) {
       actions["Take 001"].play();
     } else {
