0b138b12993cda0cca5a645dd8ec70085f1774c1
Clara <clarapachecodev@gmail.com>
2023-12-18 10:00:22 -0300

    Normalize the rotation value to ensure it stays within the range [0, 2 * Math.PI].        * The goal is to ensure that the rotation value remains within a specific range to        * prevent potential issues with very large or negative rotation values.

--- src/models/Island.jsx
+++ src/models/Island.jsx
@@ -9,7 +9,7 @@ Title: Fox's islands
 
 import { useEffect, useRef } from "react";
 import { useGLTF } from "@react-three/drei";
-import { useThree } from "@react-three/fiber";
+import { useFrame, useThree } from "@react-three/fiber";
 import { a } from "@react-spring/three";
 
 import islandScene from "../assets/assets/3d/island.glb";
@@ -91,6 +91,39 @@ const Island = ({ isRotating, setIsRotating, ...props }) => {
       document.addEventListener("keyup", handleKeyUp);
     };
   }, [gl, handlePointerDown, handlePointerUp, handlePointerMove]);
+
+  useFrame(() => {
+    if (!isRotating) {
+      rotationSpeed.current *= dampingFactor;
+
+      if (Math.abs(rotationSpeed.current) < 0.001) {
+        rotationSpeed.current = 0;
+      }
+    } else {
+      const rotation = islandRef.current.rotation.y;
+
+      const normalizedRotation =
+        ((rotation % (2 * Math.PI)) + 2 * Math.PI) % (2 * Math.PI);
+
+      // Set the current stage based on the island's orientation
+      switch (true) {
+        case normalizedRotation >= 5.45 && normalizedRotation <= 5.85:
+          setCurrentStage(4);
+          break;
+        case normalizedRotation >= 0.85 && normalizedRotation <= 1.3:
+          setCurrentStage(3);
+          break;
+        case normalizedRotation >= 2.4 && normalizedRotation <= 2.6:
+          setCurrentStage(2);
+          break;
+        case normalizedRotation >= 4.25 && normalizedRotation <= 4.75:
+          setCurrentStage(1);
+          break;
+        default:
+          setCurrentStage(null);
+      }
+    }
+  });
 
   return (
     <a.group ref={islandRef} {...props}>
