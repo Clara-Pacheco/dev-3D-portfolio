c9d8a72594bbc229919e7eb0ae89d2c2e4a0b2e5
Clara <clarapachecodev@gmail.com>
2024-01-09 10:54:55 -0300

    Add a route for Construction page and re-route the /home page

--- src/App.jsx
+++ src/App.jsx
@@ -6,15 +6,17 @@ import { About } from "./pages/About";
 import { Contact } from "./pages/Contact";
 import { Projects } from "./pages/Projects";
 import { Navbar } from "./components/Navbar";
+import { Construcao } from "./pages/Construcao";
 
 export const App = () => {
   return (
     <>
       <main className="bg-slate-300/20">
         <Router>
-          <Navbar />
+          {/* <Navbar /> */}
           <Routes>
-            <Route path="/" element={<Home />} />
+            <Route path="/" element={<Construcao />} />
+            <Route path="/home" element={<Home />} />
             <Route path="/about" element={<About />} />
             <Route path="/projects" element={<Projects />} />
             <Route path="/contact" element={<Contact />} />
